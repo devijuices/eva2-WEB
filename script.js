0b28dffc5d6b1d5a377a7a676539d76cb88f048b
devijuices <pardoevelyn2@gmail.com>
2026-02-25 07:57:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Gracias 💚</title>
+  <link rel="stylesheet" href="style.css">
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: "Short Stack", cursive;
+      font-weight: 400;
+      font-style: normal;
+      background: linear-gradient(135deg, #ff0000e9, #ffaa00, #ffd900);
+      color: white;
+      text-align: center;
+    }
+
+    .card {
+      background: rgba(0, 0, 0, 0.083);
+      width: 60%;
+      height: 60%;
+      padding:90px 20px;
+      border-radius: 20px;
+      backdrop-filter: blur(10px);
+      justify-content: center;
+      font-size: 1.5rem;
+    }
+
+    a {
+      display: inline-block;
+      margin-top: 20px;
+      padding: 10px 20px;
+      background: white;
+      color: #ff0000;
+      text-decoration: none;
+      border-radius: 10px;
+      font-weight: bold;
+    }
+
+    a:hover {
+      background: #f1f1f1;
+    }
+  </style>
+</head>
+<body>
+
+  <div class="card">
+    <h1>🍓 ¡Gracias por tu tiempo! 🥭</h1>
+    <p>
+      Tu opinión nos ayuda a seguir siendo 
+      <strong>naturalmente deliciosos </strong> 
+    </p>
+
+    <a href="index.html">Volver al inicio</a>
+  </div>
+
+</body>
+</html>
